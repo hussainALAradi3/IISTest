c122b05c6466cfd5913c4a97803eac43299c963f
Dev-Safeway <Dev-Safeway@DESKTOP-BMMK3OB>
2025-11-20 15:25:41 +0300

    test

--- test.js
+++ test.js
@@ -2,4 +2,5 @@ console.log("this is test");
 const a = 5;
 const b = 10;
 const summation = () => a + b;
+const subtraction = () => a - b;
 console.log(`The sum of ${a} and ${b} is ${summation()}`);
