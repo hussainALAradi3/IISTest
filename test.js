dd1281ec3602570185ca195faf15e87dab35b05e
Dev-Safeway <Dev-Safeway@DESKTOP-BMMK3OB>
2025-11-22 08:54:16 +0300

    test

--- test.js
+++ test.js
@@ -3,6 +3,7 @@ const a = 5;
 const b = 10;
 const summation = () => a + b;
 const subtraction = () => a - b;
+const multiplication = () => a * b;
 const dummyFunction = () => {
   return "Im  dummy function";
 };
